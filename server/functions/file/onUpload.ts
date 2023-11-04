a143535959ece6f0e4d045fccb02c0440597e3ee
Harry <harry.luff4@gmail.com>
2023-11-04 14:31:09 +0000

    Initial commit

---
+++ server/functions/file/onUpload.ts
@@ -0,0 +1,27 @@
+import * as aws from "../../lib/aws";
+import { v4 as uuidv4 } from "uuid";
+
+export const handler = async (event: any) => {
+  try {
+    const connectionId = event.requestContext.connectionId;
+    await startStepFunctionWorkflow(connectionId);
+    return {
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+    };
+  }
+};
+
+async function startStepFunctionWorkflow(connectionId: string) {
+  const params = {
+    stateMachineArn: process.env.STATE_MACHINE_ARN,
+    name: uuidv4(),
+    input: JSON.stringify({
+      connectionId,
+    }),
+  };
+  await aws.call("stepFunction", "startExecution", params);
+}
